156c5185dcb3ab55f537fe3a733ec7bf332dd376
Suraj Jadhav <surajjadhav6272@gmail.com>
2026-07-08 17:25:17 +0530

    project creation started

---
+++ src/main/java/com/suraj/hero_cycle_pricing/HeroCyclePricingApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.hero_cycle_pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroCyclePricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroCyclePricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suraj/hero_cycle_pricing/HeroCyclePricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.hero_cycle_pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroCyclePricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
